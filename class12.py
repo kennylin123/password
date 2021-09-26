6842a9a9a0503fa9f72788e55722b2e5e1a1f26c
kenny lin <kenny320555@gmail.com>
2021-09-27 01:57:58 +0800

    123456

--- class12.py
+++ class12.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 y = 0
-while True:
+while y < 3:
     x = input('請輸入密碼: ')
     if x == password:
         print('登入成功')
@@ -9,7 +9,4 @@ while True:
         y = y + 1
         print('密碼錯誤 還有', 3 - y, '次機會')
         if y == 3:
-            print('數入次數已達上限')
-            break
-
-
+            print('輸入次數已達上限')
