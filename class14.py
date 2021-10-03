702c9a21f9a5b2c205d6f18f022432e2a68de43c
kenny lin <kenny320555@gmail.com>
2021-10-03 21:20:11 +0800

    edit

--- class14.py
+++ class14.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1,100)
+start = input('請決定隨機數字範圍開始: ')
+end = input('請決定隨機數字範圍結束: ')
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
 count = 0
 while True:
     count += 1
@@ -7,9 +11,11 @@ while True:
     num = int(num)
     if num == r:
         print('你猜中了')
+        break
+        print('這是猜的第', count, '次')
     elif num < r:
         print('再大一點')
     elif num > r:
-        print('在小一點')
+        print('再小一點')
     print('這是猜的第', count, '次')
 
